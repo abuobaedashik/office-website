8cb44003aa47bee3205068dd1c2071020906926b
abuobaedashik <abuobaedashik@gmail.com>
2025-03-23 15:02:53 +0600

    edit course

--- src/Components/CourseCard.jsx
+++ src/Components/CourseCard.jsx
@@ -17,10 +17,10 @@ const CourseCard = () => {
 
   return (
     <div>
-        <div className="mt-12 w-11/12 mx-auto   text-4xl text-center font-bold  font-custom">Our Popular Course</div>
+        <div className="mt-12 w-11/12 mx-auto   text-4xl text-center font-bold  font-custom">Our Most Popular Course</div>
         <div className="mt-6 max-w-[1920px] mx-auto min-h-screen">
-      <div className=" grid grid-cols-1 md:grid-cols-3 gap-4 w-11/12 mx-auto">
-        {courses.map((course) => (
+      <div className=" grid grid-cols-1 md:grid-cols-3 gap-5 w-11/12 mx-auto">
+        {courses.slice(0, 6).map((course) => (
           <CourseCards key={course._id} course={course} />
         ))}
       </div>
